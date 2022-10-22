e7cab67f12474251557c7d3ebb882d31c0217697
Rafael Londoño Botero <athenapms12345@gmail.com>
2022-10-22 17:01:40 -0500

    Implementation for saving documents into a local DB

--- DataBase.js
+++ DataBase.js
@@ -1,7 +1,60 @@
 var localDB = new PouchDB('application');
 var remoteDB = new PouchDB('http://admin:hola123@localhost:5984/application');
-localDB.info().then(function (info) {
-    console.log(info);
+
+var createButton = document.getElementById("createButton")
+
+
+
+/* Creating a document */
+const verifyInputs = (name, lastname, cellphone, direction, stratum, email) => {
+    if (name == "" || lastname == "" || cellphone == "" || direction == "" || stratum == "NA" || email == "") {
+        return false
+    }
+    return true
+
+}
+
+const saveFile = (name, lastname, cellphone, direction, stratum, email) => {
+    let file = {
+        "_id": String(localDB.info().doc_count + 1),
+        "name": name,
+        "lastname": lastname,
+        "cellphone": cellphone,
+        "direction": direction,
+        "stratum": stratum,
+        "email": email
+    }
+
+    localDB.put(file)
+}
+
+const clearInputs = () =>{
+    document.getElementById("name").value = ""
+    document.getElementById("lastnames").value = ""
+    document.getElementById("cellphone").value = ""
+    document.getElementById("direction").value = ""
+    document.getElementById("stratum").value = "NA"
+    document.getElementById("email").value = ""
+}
+
+createButton.addEventListener("click", () => {
+    let name = document.getElementById("name").value
+    let lastname = document.getElementById("lastnames").value
+    let cellphone = document.getElementById("cellphone").value
+    let direction = document.getElementById("direction").value
+    let stratum = document.getElementById("stratum").value
+    let email = document.getElementById("email").value
+
+    let areCorrect = verifyInputs(name, lastname, cellphone, direction, stratum, email)
+
+    if (!areCorrect) {
+        alert("Por favor reingrese todos los campos")
+    } else {
+        alert("Se han enviado los datos a la base de datos local")
+        saveFile(name, lastname, cellphone, direction, stratum, email)
+    }
+    clearInputs()
+  
 })
 
-localDB.sync(remoteDB)
+
