dfc06456bc24a19189b7ce338a3579dcf887c2ae
Rafael Londoño Botero <athenapms12345@gmail.com>
2022-10-22 21:04:23 -0500

    Implementation of showing all the documents

--- DataBase.js
+++ DataBase.js
@@ -1,11 +1,19 @@
 var localDB = new PouchDB('application');
 var remoteDB = new PouchDB('http://admin:hola123@localhost:5984/application');
 
-var createButton = document.getElementById("createButton")
+
+
+localDB.info().then(function (info) {
+    console.log(info);
+})
+
+
+
 
 
 
 /* Creating a document */
+
 const verifyInputs = (name, lastname, cellphone, direction, stratum, email) => {
     if (name == "" || lastname == "" || cellphone == "" || direction == "" || stratum == "NA" || email == "") {
         return false
@@ -14,9 +22,24 @@ const verifyInputs = (name, lastname, cellphone, direction, stratum, email) => {
 
 }
 
+const makeid = (length) => {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const getDocCount = (doc_count) => {
+    return doc_count
+}
+
 const saveFile = (name, lastname, cellphone, direction, stratum, email) => {
+
     let file = {
-        "_id": String(localDB.info().doc_count + 1),
+        "_id": makeid(5),
         "name": name,
         "lastname": lastname,
         "cellphone": cellphone,
@@ -28,7 +51,7 @@ const saveFile = (name, lastname, cellphone, direction, stratum, email) => {
     localDB.put(file)
 }
 
-const clearInputs = () =>{
+const clearInputs = () => {
     document.getElementById("name").value = ""
     document.getElementById("lastnames").value = ""
     document.getElementById("cellphone").value = ""
@@ -37,24 +60,65 @@ const clearInputs = () =>{
     document.getElementById("email").value = ""
 }
 
-createButton.addEventListener("click", () => {
-    let name = document.getElementById("name").value
-    let lastname = document.getElementById("lastnames").value
-    let cellphone = document.getElementById("cellphone").value
-    let direction = document.getElementById("direction").value
-    let stratum = document.getElementById("stratum").value
-    let email = document.getElementById("email").value
 
-    let areCorrect = verifyInputs(name, lastname, cellphone, direction, stratum, email)
+let show = document.getElementById("show")
+var createButton = document.getElementById("createButton")
+if (createButton) {
+    createButton.addEventListener("click", () => {
+        let name = document.getElementById("name").value
+        let lastname = document.getElementById("lastnames").value
+        let cellphone = document.getElementById("cellphone").value
+        let direction = document.getElementById("direction").value
+        let stratum = document.getElementById("stratum").value
+        let email = document.getElementById("email").value
 
-    if (!areCorrect) {
-        alert("Por favor reingrese todos los campos")
-    } else {
-        alert("Se han enviado los datos a la base de datos local")
-        saveFile(name, lastname, cellphone, direction, stratum, email)
-    }
-    clearInputs()
-  
-})
+        let areCorrect = verifyInputs(name, lastname, cellphone, direction, stratum, email)
 
-
+        if (!areCorrect) {
+            alert("Por favor reingrese todos los campos")
+        } else {
+            alert("Se han enviado los datos a la base de datos local")
+            saveFile(name, lastname, cellphone, direction, stratum, email)
+        }
+        clearInputs()
+    })
+} else if (show) {
+    show.addEventListener("click", () => {
+        localDB.allDocs({ include_docs: true }).then(function (docs) {
+            console.log(docs.rows);
+            let tbody = document.getElementById("tbody")
+            for (let i = 0; i < docs.rows.length; i++) {
+                let tr = document.createElement("tr")
+                for (let j = 0; j < 7; j++) {
+                    let td = document.createElement("td")
+                    switch (j) {
+                        case 0:
+                            td.innerHTML = docs.rows[i].doc._id
+                            break
+                        case 1:
+                            td.innerHTML = docs.rows[i].doc.name
+                            break
+                        case 2:
+                            td.innerHTML = docs.rows[i].doc.lastname
+                            break
+                        case 3:
+                            td.innerHTML = docs.rows[i].doc.cellphone
+                            break
+                        case 4:
+                            td.innerHTML = docs.rows[i].doc.direction
+                            break
+                        case 5:
+                            td.innerHTML = docs.rows[i].doc.email
+                            break
+                        case 6:
+                            td.innerHTML = docs.rows[i].doc.stratum
+                            break
+                    }
+                    tr.appendChild(td)
+                }
+                tbody.appendChild(tr)
+            }
+        })
+        show.disabled = true
+    })
+}
